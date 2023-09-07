304e67f21973708b6e50893a5eeb46efb7e97615
Seth Hogue <shogue21@basecampcodingacademy.org>
2023-09-07 16:48:39 -0400

    update to node version 18, minor changes for readability

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,8 +7,8 @@ const Navbar = () => {
         <nav className={`navbar navbar-expand-lg navbar-light ${styles.nav}`}>
             <div className="container-fluid justify-content-between">
                 <div className="d-flex align-items-center">
-                    <Link href="/"><a className={styles.noDecor}><img className={styles.logo} src="logo.gif"></img></a></Link>
-                    <Link href="/"><a className={`${styles.noDecor} ${styles.header} align-middle`}>Yalobusha County Crime Stoppers</a></Link>
+                    <Link legacyBehavior href="/"><a className={styles.noDecor}><img className={styles.logo} src="logo.gif"></img></a></Link>
+                    <Link legacyBehavior href="/"><a className={`${styles.noDecor} ${styles.header} align-middle`}>Yalobusha County Crime Stoppers</a></Link>
                 </div>
                 <div>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,7 +16,7 @@ const Navbar = () => {
                     </button>
                     <div className={`collapse navbar-collapse`} id="navbarNavAltMarkup">
                     <div className={`${styles.collapsed} navbar-nav`}>
-                        <Link href="/about"><a className={`${styles.link}`} aria-current="page">About</a></Link>
+                        <Link legacyBehavior href="/about"><a className={`${styles.link}`} aria-current="page">About</a></Link>
                     </div>
                     </div>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,7 @@ import styles from '../styles/home.module.css'
 import { useState } from 'react'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
-import {TwitterFollowButton} from 'react-twitter-embed';
-import BackgroundSlider from 'react-background-slider';
+
 
 const index = () => {
   const [isAboutHovered, setIsAboutHovered] = useState(false);
@@ -23,12 +21,23 @@ const index = () => {
     <div className={styles.landing}>
       <div className={styles.overlay}></div>
       <div className={styles.center}>
-      <BackgroundSlider images={["watervalley1.jpg", "coffeeville1.jpeg", "oakland1.jpeg", "coffeeville2.jpeg", "oakland2.jpeg", "watervalley2.jpeg", "coffeeville3.jpeg", "yalobusha.jpeg", "oakland3.jpeg", "coffeeville4.jpeg"]} transition={1} duration={5}/>
+      <div className={styles.slider}>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+        <div className={styles.slide}></div>
+      </div>
         <div className={styles.main}>
         <h1 className={styles.text}>Yalobusha County Crime Stoppers</h1>
         <p className={styles.slogan}>If you see something, or hear something, say something!</p>
         <div className={styles.buttons}>
-          <Link href="about#about">
+          <Link legacyBehavior href="about#about">
             <motion.a className={isAboutHovered ? styles.transparent_btn_Hover : styles.button} 
             onMouseEnter={toggleAboutHover} 
             onMouseLeave={toggleAboutHover}
@@ -36,7 +45,7 @@ const index = () => {
             variants={variants}>
             About Us</motion.a>
           </Link>
-          <Link href="about#tip_process">
+          <Link legacyBehavior href="about#tip_process">
             <motion.a className={isProcessHovered ? styles.transparent_btn_Hover : styles.button} 
             onMouseEnter={toggleProcessHover} 
             onMouseLeave={toggleProcessHover}
